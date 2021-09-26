0a012647808d6467f5a8e4d1d6074cc9decb56f3
Manupkm7 <manuelespinoza238@gmail.com>
2021-09-26 04:42:52 -0300

    added skills and styles from skills

--- src/Components/Body/Skills/Skills.jsx
+++ src/Components/Body/Skills/Skills.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import './Skills.css'
+import {SkillsData} from '../../Data/Skills'
+import Separator from '../../Common/Separator/Separator'
+import SkillCard from './SkillCard'
+
 const Skills = () => {
+    const data = SkillsData
     return (
-        <div>
-            
+        <div className='skills'>
+            <Separator />
+            <label className='section-title'>Tecnologias</label>
+            <div className="skills-container">
+                {data.map((item)=>{
+                    return (
+                        <div className='skills-section'>
+                            <label className='skills-section-title'>{item.type}</label>
+                            <div className="skills-list">
+                                {item.list.map((skill)=>{
+                                    return <SkillCard skill={skill} />
+                                })}
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
---
+++ src/Components/Data/Skills.js
@@ -0,0 +1,57 @@
+export const SkillsData = [
+    {
+        type: 'Frontend',
+        list:[{
+            name:'React Js',
+            icon: <i class="devicon-react-original-wordmark colored"></i>
+        },
+        {
+            name:'CSS',
+            icon: <i class="devicon-css3-plain-wordmark colored"></i>
+
+        },
+        
+        {
+            name:'Material UI',
+            icon: <i class="devicon-materialui-plain colored"></i>
+        },
+        {
+            name:'Redux',
+            icon: <i class="devicon-redux-original colored"></i>
+        },
+        {
+            name:'Javascript',
+            icon: <i class="devicon-javascript-plain colored"></i>
+        },
+        {
+            name: 'Bootstrap',
+            icon: <i class="devicon-bootstrap-plain-wordmark colored"></i>
+        }
+    ]   
+    },
+    {
+    type: 'Backend',
+    list:[
+    {
+        name: 'NodeJs',
+        icon: <i class="devicon-nodejs-plain-wordmark colored"></i>
+    },
+    {
+        name: 'ExpressJs',
+        icon: <i class="devicon-express-original-wordmark colored"></i>
+    },
+    {
+        name: 'Typescript',
+        icon: <i class="devicon-typescript-plain colored"></i>
+    },
+    {
+        name: 'PostgreSql',
+        icon: <i class="devicon-postgresql-plain-wordmark colored"></i>
+    },
+    {
+        name: 'MongoDB',
+        icon: <i class="devicon-mongodb-plain-wordmark colored"></i>
+    }
+]
+}
+]
